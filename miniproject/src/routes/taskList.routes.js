bb473a5ef0b44ea1eab33839af1eb4bfe07a9ba0
GabrielRER <gabrielramonevangelistaramos@gmail.com>
2024-07-23 15:54:07 -0300

    comments add

--- miniproject/src/routes/taskList.routes.js
+++ miniproject/src/routes/taskList.routes.js
@@ -3,16 +3,22 @@ import { createTask, showList, searchTask, taskDone, taskNotDone, doneUpdate, de
 
 const listRouter = Router();
 
+// Criar tarefa
+
 listRouter.post("/createtask", (req,res) => {
     const {task, deadline, tag, done} = req.body;
     const newTask = createTask(task, deadline, tag, done);
     res.status(200).json({ newTask });
 });
 
+// Listar tarefas
+
 listRouter.get("/listtask", (req, res) => {
     const listTask = showList();
     res.status(200).json({ listTask });
 });
+
+// Procurar Tarefa
 
 listRouter.get("/search/:id", (req, res) => {
     const { id } = req.params;
@@ -20,21 +26,29 @@ listRouter.get("/search/:id", (req, res) => {
     res.status(200).json({ search });
 });
 
+// Listar tarefas concluídas
+
 listRouter.get("/taskdone", (req, res) => {
     const taskCompleted = taskDone();
     res.status(200).json({ taskCompleted });
 });
+
+// Listar tarefas não concluídas
 
 listRouter.get("/tasknotdone", (req, res) => {
     const taskIncompleted = taskNotDone();
     res.status(200).json({ taskIncompleted });
 })
 
+// Mudar status de conclusão de tarefa
+
 listRouter.patch("/changedone/:id", (req, res) => {
     const { id } = req.params;
     const update = doneUpdate(id);
     res.status(200).json({ update });
 })
+
+// Deletar tarefa
 
 listRouter.delete("/deletetask/:id", (req,res) => {
     const { id } = req.params;
